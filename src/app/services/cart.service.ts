5c1bceaf9d50512948d7e778260e88fa139b10c2
dennis <wakahiad@gmail.com>
2022-05-13 22:23:23 +0300

    increment, decrement and remove cart products quantity

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -61,4 +61,20 @@ export class CartService {
   }
 
 
+  decrementQuantity(cartItem: CartItem) {
+    cartItem.quantity--;
+    if (cartItem.quantity === 0) {
+      this.remove(cartItem);
+    } else {
+      this.computeCartTotals();
+    }
+  }
+
+  remove(cartItem: CartItem) {
+    const itemIndex = this.cartItems.findIndex(tempCartItem => tempCartItem.id == cartItem.id);
+    if (itemIndex > -1) {
+      this.cartItems.splice(itemIndex, 1);
+      this.computeCartTotals();
+    }
+  }
 }
