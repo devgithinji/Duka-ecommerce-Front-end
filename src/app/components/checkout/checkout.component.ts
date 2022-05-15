a7ef66636a5512e195e86717f6e5cbea7af821ee
dennis <wakahiad@gmail.com>
2022-05-15 11:15:22 +0300

    checkout form setup

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup} from "@angular/forms";
 import {CustomFormService} from "../../services/custom-form.service";
+import {Country} from "../../common/country";
+import {State} from "../../common/state";
 
 @Component({
   selector: 'app-checkout',
@@ -13,6 +15,7 @@ export class CheckoutComponent implements OnInit {
   totalQuantity: number = 0;
   creditCardYears: number[] = [];
   creditCardMonths: number[] = [];
+  countries: Country[] = [];
 
   checkoutFormGroup: FormGroup = this.formBuilder.group({
     customer: this.formBuilder.group({
@@ -44,6 +47,9 @@ export class CheckoutComponent implements OnInit {
     })
   });
 
+  shippingAddressStates: State[] = [];
+  billingAddressStates: State[] = [];
+
   constructor(private formBuilder: FormBuilder, private customFormService: CustomFormService) {
   }
 
@@ -54,6 +60,9 @@ export class CheckoutComponent implements OnInit {
     )
     this.customFormService.getCreditCardYears().subscribe(
       data => this.creditCardYears = data
+    )
+    this.customFormService.getCountries().subscribe(
+      data => this.countries = data
     )
   }
 
@@ -67,8 +76,46 @@ export class CheckoutComponent implements OnInit {
 
     if (isChecked) {
       this.checkoutFormGroup.controls.billingAddress.setValue(this.checkoutFormGroup.controls.shippingAddress.value)
+      this.billingAddressStates = this.shippingAddressStates;
     } else {
       this.checkoutFormGroup.controls.billingAddress.reset();
+      this.billingAddressStates = [];
     }
+  }
+
+  handleMonthsAndYears() {
+    const creditCardFormGroup = this.checkoutFormGroup.get('creditCard');
+    const currentYear: number = new Date().getFullYear();
+    const selectedYear: number = Number(creditCardFormGroup?.value.expirationYear)
+
+    let startMonth: number;
+
+    if (currentYear === selectedYear) {
+      startMonth = new Date().getMonth() + 1;
+    } else {
+      startMonth = 1;
+    }
+
+    this.customFormService.getCreditCardMonths(startMonth).subscribe(
+      data => this.creditCardMonths = data
+    )
+  }
+
+  getStates(formGroupName: string) {
+    const formGroup = this.checkoutFormGroup.get(formGroupName);
+    const countryCode = formGroup?.value.country.code;
+
+    this.customFormService.getStates(countryCode).subscribe(
+      data => {
+        if (formGroupName === 'shippingAddress') {
+          this.shippingAddressStates = data;
+
+        } else {
+          this.billingAddressStates = data;
+        }
+
+        formGroup?.get('state')?.setValue(data[0]);
+      }
+    )
   }
 }
--- src/app/services/custom-form.service.ts
+++ src/app/services/custom-form.service.ts
@@ -1,12 +1,33 @@
 import {Injectable} from '@angular/core';
-import {Observable, of} from "rxjs";
+import {map, Observable, of} from "rxjs";
+import {HttpClient} from "@angular/common/http";
+import {Country} from "../common/country";
+import {State} from "../common/state";
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomFormService {
 
-  constructor() {
+  private countriesUrl = 'http://localhost:8080/api/countries';
+  private statesUrl = 'http://localhost:8080/api/states';
+
+  constructor(private httpClient: HttpClient) {
+  }
+
+  getStates(countryCode: string): Observable<State[]> {
+    const searchStatesUrl = `${this.statesUrl}/search/findByCountryCode?code=${countryCode}`;
+
+    return this.httpClient.get<GetStatesResponse>(searchStatesUrl).pipe(
+      map(response => response._embedded.states)
+    )
+  }
+
+  getCountries(): Observable<Country[]> {
+    return this.httpClient.get<GetResponseCountries>(this.countriesUrl).pipe(
+      map(response => response._embedded.countries)
+    )
+
   }
 
   getCreditCardMonths(startMonth: number): Observable<number[]> {
@@ -21,9 +42,21 @@ export class CustomFormService {
     let data: number[] = [];
     const startYear: number = new Date().getFullYear();
     const endYear: number = startYear + 10;
-    for (let theYear = startYear; theYear <= endYear ; theYear++){
+    for (let theYear = startYear; theYear <= endYear; theYear++) {
       data.push(theYear);
     }
     return of(data);
+  }
+}
+
+interface GetResponseCountries {
+  _embedded: {
+    countries: Country[]
+  }
+}
+
+interface GetStatesResponse {
+  _embedded: {
+    states: State[]
   }
 }
