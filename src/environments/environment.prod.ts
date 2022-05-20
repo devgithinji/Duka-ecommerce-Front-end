dc380e382f3c2fa3696500d2f7741e0bb1d47dc6
dennis <wakahiad@gmail.com>
2022-05-20 17:37:52 +0300

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: ' https://duka-backend.herokuapp.com/api',
+  stripe_publishable_key: 'pk_test_51Ifn23KAfCyVQ4sYRbXZTUXKpUSHD71SZJs4RT5RhWFyZ2oZLoGA5sbV4NYQpSb8rsKizh0NQ7XEy5ZMTTqjX19p00KJDJH7gj'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'https://localhost:8443/api',
+  apiUrl: ' https://duka-backend.herokuapp.com/api',
   stripe_publishable_key: 'pk_test_51Ifn23KAfCyVQ4sYRbXZTUXKpUSHD71SZJs4RT5RhWFyZ2oZLoGA5sbV4NYQpSb8rsKizh0NQ7XEy5ZMTTqjX19p00KJDJH7gj'
 };
 
