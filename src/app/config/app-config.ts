534b6c70df92e2d9dc1d9af9f9b016966112aead
dennis <wakahiad@gmail.com>
2022-05-20 18:09:22 +0300

    deployment fix

--- src/app/config/app-config.ts
+++ src/app/config/app-config.ts
@@ -2,7 +2,7 @@ export default {
   oidc: {
     clientId: '0oa5305rlqs9zEgLJ5d7',
     issuer: 'https://dev-71040941.okta.com/oauth2/default',
-    redirectUri: 'https://localhost:4200/login/callback',
+    redirectUri: 'https://duka-app.herokuapp.com/login/callback',
     scopes: ['openid', 'profile', 'email']
   }
 }
